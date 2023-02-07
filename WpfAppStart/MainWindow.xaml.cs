dbf6516d5696c5ebe83510ed980b926b61b4b8e9
User <User@WIN-5MR7695IMJG>
2023-02-07 21:29:50 +0300

    Добавьте файлы проекта.

---
+++ WpfAppStart/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppStart
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> list = new List<string>()
+            {
+                "🐙", "🐙",
+                "🐸","🐸",
+                "🐘","🐘",
+                "🐳","🐳",
+                "🐪","🐪",
+                "🦕","🦕",
+                "🦘","🦘",
+                "🦔","🦔"
+            };
+            Random random= new Random();
+            foreach (TextBlock tbl in mainGrid.Children.OfType<TextBlock>()) 
+            {
+                int index=random.Next(list.Count);
+                string nextEmoji = list[index];
+                tbl.Text = nextEmoji;
+                list.RemoveAt(index);
+            }
+        }
+    }
+}
